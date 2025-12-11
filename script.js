3c2bd7cf2723ee0498385221a22d807daccc6117
s0lix-x <dgarou2@gmail.com>
2025-12-12 00:01:21 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const words = ["Front-End Developer", "UI/UX Designer", "Back-End Developer"];
+let w = 0; 
+let i = 0;
+
+function type() {
+  const current = words[w];
+  if (i < current.length) {
+    document.getElementById("typewriter").textContent += current[i];
+    i++;
+    setTimeout(type, 80);
+  } else {
+    setTimeout(() => erase(), 1000);
+  }
+}
+
+function erase() {
+  const current = words[w];
+  if (i > 0) {
+    document.getElementById("typewriter").textContent = current.substring(0, i - 1);
+    i--;
+    setTimeout(erase, 50);
+  } else {
+    w = (w + 1) % words.length;
+    setTimeout(type, 300);
+  }
+}
+
+type();
+
+window.addEventListener("load", () => {
+  const form = document.querySelector("form");
+  if (form) form.reset();
+});
